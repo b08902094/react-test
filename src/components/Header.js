239d94feb9cee1f6f4455e4415e11ac1589d6213
Marica Takahashi <b08902094@ntu.edu.tw>
2024-02-17 09:55:00 +0900

    [Finish] Navbar

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+const Header = () => {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary" sticky="top">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/blog">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
---
+++ src/components/HomePage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+
+const HomePage = () => {
+  return <div>HomePage</div>;
+};
+
+export default HomePage;
